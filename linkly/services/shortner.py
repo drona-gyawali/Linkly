911999e4c4a7e7352bec0e769361dce7900004c1
drona-gyawali <dronarajgyawali@gmail.com>
2025-07-17 14:30:42 +0545

    Frustation at peak

--- linkly/services/shortner.py
+++ linkly/services/shortner.py
@@ -15,8 +15,8 @@ from linkly.settings import settings
 from linkly.utils.dtype import PyObjectId
 from linkly.utils.encode_url import ShortIdGenerator
 
-redis_client = redis.from_url(settings.redis_url)
-
+redis_url = settings.redis_url.replace("redis://", "rediss://")
+redis_client = redis.from_url(redis_url)
 
 async def shorten_url(
     original_url: str,
